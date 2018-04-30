955481883ba9342b1a192657121926f2dc070182
tiantang_88 <466684860@qq.com>
2018-04-30 21:47:38 +0000

    Updates database configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+require __DIR__.'/helpers.php';
 /*
 |--------------------------------------------------------------------------
 | Create The Application
@@ -51,5 +52,5 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
-require __DIR__.'/helpers.php';
+
 return $app;
